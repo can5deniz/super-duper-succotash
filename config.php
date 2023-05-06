2c0fc0d06a5fe9f5bff35f5cebcb9f2ac99b257c
Can 5Deniz <131861022+can5deniz@users.noreply.github.com>
2023-05-06 16:01:33 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+    //declare and initialize variables
+    $title= '';
+    $seperator= '';
+    $description= '';
+
+    //set $maintance to true
+    $maintance = false;
+
+    //database connection
+
+    //database server
+    $dbserver = 'us-cdbr-east-06.cleardb.net';
+
+    //database username
+    $dbusername = 'b21c1ce0723288';
+
+    //database password
+    $dbpassword = 'f06f9991';
+
+    //database name
+    $db = 'heroku_0557db19b57d2b0';
+
+?>
