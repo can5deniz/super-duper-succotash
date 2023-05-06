3e0be8b9b7d073be77762c5dad4a28a2972bacbd
Can 5Deniz <131861022+can5deniz@users.noreply.github.com>
2023-05-06 16:43:02 +0300

    Update config.php

--- config.php
+++ config.php
@@ -18,5 +18,5 @@
     //connection settings
 
     //open connection to mysql db
-    $connection = mysqli_connect($dbserver,$dbusername,$dbpassword,$db) or die("Error " . mysqli_error($connection));
+    $connection = mysqli_connect($dbserver,$dbusername,$dbpassword,$db) or die("Error E");
 
