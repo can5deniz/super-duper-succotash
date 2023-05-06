bd5f8d71d2166e9bf38e5a9ba88c22296318932e
Can 5Deniz <131861022+can5deniz@users.noreply.github.com>
2023-05-06 16:39:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Test
+<?php
+include("config.php");
+$query = $connect->query("SELECT * FROM fruits ");
+?>
+
+<table border="1">
+  <tr>
+    <td>Not</td>
+    <td>Fruit Name</td>
+    <td>Publisher Name</td>
+  </tr>
+  <?php
+  $no = 1;
+  while($row = $query->fetch_assoc()){
+    echo "<tr>
+      <td>$no</td>
+      <td>{$row['name']}</td>
+      <td>{$row['publisher']}</td>
+    </tr>";
+    $no++;
+  }
+  ?>
+</table>
