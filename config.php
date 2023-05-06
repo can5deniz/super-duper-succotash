844c80e825d1c111a12e55df6a7038e39805d944
Can 5Deniz <131861022+can5deniz@users.noreply.github.com>
2023-05-06 16:03:27 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php
 
-    //declare and initialize variables
-    $title= '';
-    $seperator= '';
-    $description= '';
-
     //set $maintance to true
     $maintance = false;
 
@@ -21,5 +16,31 @@
 
     //database name
     $db = 'heroku_0557db19b57d2b0';
+
+
+    //connection settings
+
+    //open connection to mysql db
+    $connection = mysqli_connect($dbserver,$dbusername,$dbpassword,$db) or die("Error " . mysqli_error($connection));
+    //check connection
+    if($connection){
+        echo "Connected";
+    }else{
+        echo "Not Connected";
+    }
+
+
+    //general settings
+
+    //declare and initialize variables
+    $title= 'MORTS';
+    $seperator= ' . ';
+    $description= 'My awesome MORTS game';
+
+
+    //technical settings
+
+    //set $maintance to true
+    $maintance = false;
 
 ?>
