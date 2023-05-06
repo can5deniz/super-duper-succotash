932085d2dd151b059d0313c09ea21e3ac0cc958c
Can 5Deniz <131861022+can5deniz@users.noreply.github.com>
2023-05-06 16:41:34 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
 
-    //set $maintance to true
-    $maintance = false;
-
     //database connection
 
     //database server
@@ -22,25 +19,6 @@
 
     //open connection to mysql db
     $connection = mysqli_connect($dbserver,$dbusername,$dbpassword,$db) or die("Error " . mysqli_error($connection));
-    //check connection
-    if($connection){
-        echo "Connected";
-    }else{
-        echo "Not Connected";
-    }
 
-
-    //general settings
-
-    //declare and initialize variables
-    $title= 'MORTS';
-    $seperator= ' . ';
-    $description= 'My awesome MORTS game';
-
-
-    //technical settings
-
-    //set $maintance to true
-    $maintance = false;
 
 ?>
